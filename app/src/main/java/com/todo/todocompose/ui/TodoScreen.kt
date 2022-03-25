83302a5a87ed12b0bec9f33bd6d6a6f87bd56622
petergodwin <goddypete57@gmail.com>
2022-03-25 10:53:05 +0100

    first commit

---
+++ app/src/main/java/com/todo/todocompose/ui/TodoScreen.kt
@@ -0,0 +1,13 @@
+package com.todo.todocompose.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FullScreen(){
+    Box(modifier=Modifier.fill()) {
+        
+    }
+}
