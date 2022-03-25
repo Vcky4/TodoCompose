2a86abee6f0137a2b28f6fe95dd5e5bd24656356
petergodwin <goddypete57@gmail.com>
2022-03-25 12:09:22 +0100

    first commit

--- app/src/main/java/com/todo/todocompose/MainActivity.kt
+++ app/src/main/java/com/todo/todocompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.todo.todocompose
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -49,7 +52,6 @@ fun AddTodoDialog(){
 @Composable
 fun Title(text: String, size: Int){
     Text(text = text,
-    modifier = Modifier.fillMaxWidth(),
         style = TextStyle(
             fontSize = size.sp,
             fontWeight = FontWeight.Bold
@@ -57,11 +59,22 @@ fun Title(text: String, size: Int){
     )
 }
 
-/*
+@Composable
+fun checkbox(){
+    val isChecked = remember { mutableStateOf(false) }
+    Checkbox(
+        checked = isChecked.value,
+        onCheckedChange = {
+            isChecked.value = it
+        },
+    )
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TodoComposeTheme {
-        MainUi()
+        checkbox()
     }
-}*/
+}
---
+++ app/src/main/java/com/todo/todocompose/ui/TodoItem.kt
@@ -0,0 +1,45 @@
+package com.todo.todocompose.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.todo.todocompose.Title
+
+@Composable
+fun FullScreen(){
+    Box(modifier = androidx.compose.ui.Modifier.fillMaxSize()) {
+        
+    }
+}
+
+@Composable
+fun todoItem(title:String,){
+    Card(modifier = Modifier
+       ) {
+        Row( horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth() .padding(8.dp)) {
+            Title(text = title, size = 16)
+            val isChecked = remember { mutableStateOf(false) }
+            Checkbox(
+                checked = isChecked.value,
+                onCheckedChange = {
+                    isChecked.value = it
+                },
+
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun todo() {
+    todoItem(title = "eat fried rice today ")
+}
